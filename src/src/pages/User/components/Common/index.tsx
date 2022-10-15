5024a86cd4351864f073e22365ac6c04308cab8d
998-changyuan <1458283943@qq.com>
2022-10-15 17:05:13 +0800

    个人中心快完成了

---
+++ src/src/pages/User/components/Common/index.tsx
@@ -0,0 +1,15 @@
+/*
+ * @Author: hcy
+ * @Date: 2022-10-15 16:59:45
+ * @LastEditors: hcy
+ * @LastEditTime: 2022-10-15 16:59:49
+ * @FilePath: \src\src\pages\User\components\Common\index.tsx
+ * @Description:
+ * 
+ */
+import React from 'react'
+export default () => {
+  return (
+    <div>common</div>
+  )
+}
--- src/src/pages/User/components/MyCases/index.tsx
+++ src/src/pages/User/components/MyCases/index.tsx
@@ -2,37 +2,41 @@
  * @Author: hcy
  * @Date: 2022-10-13 19:33:22
  * @LastEditors: hcy
- * @LastEditTime: 2022-10-14 22:07:05
+ * @LastEditTime: 2022-10-15 16:47:04
  * @FilePath: \src\src\pages\User\components\MyCases\index.tsx
  * @Description: 
  * 
  */
 import React from 'react'
 import style from './index.less'
+import {
+  EyeOutlined
+} from '@ant-design/icons';
+import { Pagination ,Avatar} from 'antd';
 export default () => {
   const listMenu = [{
     title: 'java高级技术开发',
-    msg: '这是一个用java开发网站的案例！',
+    auth: '小李子',
     imgUrl:require('@/assets/user_case.jpg')
   },{
     title: 'java高级技术开发',
-    msg: '这是一个用java开发网站的案例！',
+    auth: '小冬冬',
     imgUrl:require('@/assets/user_case.jpg')
   },{
     title: 'java高级技术开发',
-    msg: '这是一个用java开发网站的案例！',
+    auth: '小欢欢',
     imgUrl:require('@/assets/user_case.jpg')
   },{
     title: 'java高级技术开发',
-    msg: '这是一个用java开发网站的案例！',
+    auth: '小鹏子',
     imgUrl:require('@/assets/user_case.jpg')
   },{
     title: 'java高级技术开发',
-    msg: '这是一个用java开发网站的案例！',
+    auth: '科科',
     imgUrl:require('@/assets/user_case.jpg')
   },{
     title: 'java高级技术开发',
-    msg: '这是一个用java开发网站的案例！',
+    auth: 'lpc',
     imgUrl:require('@/assets/user_case.jpg')
   },
   ]
@@ -41,13 +45,20 @@ export default () => {
       {
         listMenu.map((e,index) => {
           return (<div className={style.ele}>
-                <div className={style.img} key={index} style={{ backgroundImage: `url(${e.imgUrl})` }}>          
+            <div className={style.img} key={index} style={{ backgroundImage: `url(${e.imgUrl})` }}>   
+              <div className={style.mask}>
+                <div className={style.EyeOutlined }><EyeOutlined   /></div>
+              </div>
             </div>
-            <div>{e.title}</div>
-            <div>{ e.msg}</div>
+            <div style={{fontSize:'1.5rem',fontWeight:'500'}}>{e.title}</div>
+            <div style={{fontSize:'1.2rem',fontWeight:'550'}}> <Avatar
+        src="https://joeschmoe.io/api/v1/random"
+        size={{ xs: 12, sm: 12, md: 20, lg: 20, xl: 20, xxl: 20 }}
+      />{ e.auth}</div>
           </div> )
         })
       }
+      <div><Pagination pageSize={6} total={50} responsive={true} /></div> 
     </div>
   )
 }
