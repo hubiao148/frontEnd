edfe9a27a3f7d89024d38b889bda7a78fc5f1b7a
998-changyuan <1458283943@qq.com>
2022-12-24 22:48:43 +0800

    技术问答最后的接口

--- src/src/api/myShare/hotComment.ts
+++ src/src/api/myShare/hotComment.ts
@@ -2,16 +2,16 @@
  * @Author: hcy
  * @Date: 2022-12-08 19:17:43
  * @LastEditors: hcy
- * @LastEditTime: 2022-12-08 19:17:54
+ * @LastEditTime: 2022-12-24 22:26:30
  * @FilePath: \src\src\api\myShare\hotComment.ts
  * @Description:热门评论
  *
  */
 import request from '../api';
 
-export function hotComment() {
+export function hotComment(orderMode:number) {
   return request({
     method: 'get',
-    url: '/techqa/comment/list',
+    url: `/techqa/comment/list?orderMode=${orderMode}`,
   });
 }
